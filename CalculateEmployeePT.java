9f0b869cb1a6bccaaf8db4551963dc1c2b8f26cc
Alternade <aptsahu2@gmail.com>
2021-08-11 11:49:14 +0530

    UC09 - Calculate Employee part Time

---
+++ CalculateEmployeePT.java
@@ -0,0 +1,32 @@
+public class CalculateEmployeePT {
+		static int wagePerHr=20;
+		static int hrPerDay=8;
+		static int wage;
+		static String status;
+		public static void main(String[] args) {
+			String status=CheckEmp();
+			System.out.println(status);
+			System.out.println(wage);
+		}
+
+		public static String CheckEmp(){
+			int ranFunction= (int) Math.floor(Math.random()*3);
+			if(ranFunction == 1) {
+				status="FullTime";
+				wage=calculateWage(wagePerHr,hrPerDay);
+			}else if(ranFunction == 2) {
+				status="PartTime";
+				wage=calculateWage(wagePerHr,hrPerDay);
+			}else {status="Absent";}
+				
+			return status;
+		}
+		public static int calculateWage(int wagePerHr,int hrPerDay){
+			if(status == "FullTime") {
+				wage=wagePerHr*hrPerDay;
+			}else if (status == "PartTime") {
+				wage=wagePerHr*(hrPerDay/2);
+			}else if(status=="Absent") {wage=0;};
+			return wage;
+		}
+	}
